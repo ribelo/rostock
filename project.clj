5dbbc3886e460fcf5aaeebfdce467c02b6a2a02b
Ribelo <r.krzywaznia@gmail.com>
2019-03-29 17:42:15 +0100

    fix bug, for realz

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject ribelo/visby "0.1.1-SNAPSHOT"
-  :url "https://github.com/ribelo/ribelo.visby"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:install :user :project]}
-  :global-vars {*warn-on-reflection* true})
